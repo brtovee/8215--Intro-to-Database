1e7017960f66060eda6a2720d2578944455f919c
Bruce Tovee <37085952+brtovee@users.noreply.github.com>
2018-03-05 16:31:25 -0500

    Add files via upload

---
+++ Lab3/Lab3BruceTovee.sql
@@ -0,0 +1,33 @@
+﻿--QUERY 1
+--1a
+SELECT * FROM Part_T;
+--1b
+SELECT part, material, size, cost FROM Part_T;
+--1c
+SELECT part, material, cost FROM Part_T;
+
+
+--QUERY 2
+--2a
+SELECT part, material, cost FROM Part_T WHERE cost < 1.00;
+--2b
+SELECT part, size, cost FROM Part_T WHERE cost>2.10;
+
+--QUERY 3
+--3a
+SELECT AVG(cost) FROM Part_T WHERE material = 'BRASS';
+--3b
+SELECT AVG(cost) FROM Part_T WHERE part = 'SCREW';
+
+--QUERY 4
+--4a
+SELECT SUBSTRING(part FROM 1 FOR 3) FROM Part_T;
+--4b
+SELECT SUBSTRING(material FROM 1 FOR 5) FROM Part_T;
+
+--QUERY 5
+--5a
+INSERT INTO Part_T(part, material, size, cost) VALUES('NAIL', 'STEEL', 'SMALL', 0.15);
+--5b
+INSERT INTO Part_T(part, material, size, cost) VALUES('HAMMER', 'STEEL', 'SMALL', 7.15);
+
