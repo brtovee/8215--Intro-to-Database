75704919153cbbc1c8d114ce2b06b7d04184ea22
Bruce Tovee <37085952+brtovee@users.noreply.github.com>
2018-03-05 16:32:50 -0500

    Add files via upload

---
+++ Assignment1/Assignment1_SDG.DDL.sql
@@ -0,0 +1,34 @@
+﻿/*
+File Name: Assignment1_SDG.DDL
+Authors: Bruce Tovee, Johnathan Gonzalez
+Team Name: Team J-uce
+Date Created: 2018-02-14
+*/
+
+DROP TABLE IF EXISTS Indicator_t;
+CREATE TABLE Indicator_t(
+	Goal INTEGER,
+	Target VARCHAR(3),
+	IndicatorRef VARCHAR(5),
+	IndicatorID VARCHAR(7),
+	IndicatorDescription VARCHAR,
+	CONSTRAINT Indicator_PK PRIMARY KEY(IndicatorID)
+);
+
+
+
+DROP TABLE IF EXISTS Country_t;
+CREATE TABLE Country_t(
+	CountryOrAreaCode VARCHAR,
+	CountryOrAreaName VARCHAR,
+	CONSTRAINT Country_PK PRIMARY KEY(CountryOrAreaCode)
+);
+
+DROP TABLE IF EXISTS Series_t;
+CREATE TABLE Series_t(
+	SeriesCode VARCHAR(11),
+	SeriesType VARCHAR(2),
+	SeriesDescription VARCHAR,
+	CONSTRAINT Series_PK PRIMARY KEY(SeriesCode)
+);
+	
---
+++ Assignment1/Assignment1_SDG.DML.sql
@@ -0,0 +1,15 @@
+﻿/*
+File Name: Assignment1_SDG.DML
+Authors: Bruce Tovee, Johnathan Gonzalez
+Team Name: Team J-uce
+Date Created: 2018-02-14
+*/
+
+DELETE FROM Indicator_t;
+
+INSERT INTO Indicator_t( Goal, Target, IndicatorRef, IndicatorID, IndicatorDescription) VALUES( 1, '1.1', '1.1.1', 'C010101', 'Proportion of population below the international poverty line, by sex, age, employment status and geographical location (urban/rural)' );
+
+DELETE FROM Series_t;
+
+INSERT INTO Series_t(SeriesCode, SeriesType, SeriesDescription) VALUES('SI_POV_DAY1', 'SD', 'Proportion of population below the international poverty line of US$1.90 per day');
+INSERT INTO Series_t(SeriesCode, SeriesType, SeriesDescription) VALUES('SI_POV_EMP1', 'SD', 'Proportion of employed population below the international poverty line of US$1.90 per day (the working poor)');
