7c9b5b94002660d609de6a7cf50e990a5a661eb0
Bruce Tovee <37085952+brtovee@users.noreply.github.com>
2018-03-06 23:01:15 -0500

    Add files via upload

---
+++ Lab7a/Lab7a.sql
@@ -0,0 +1,37 @@
+﻿--Exercise 1
+SELECT
+	Country.Code,
+	Country.Name,
+	City.Name
+FROM
+	Country INNER JOIN City
+ON
+	Country.Code = City.CountryCode;
+	
+--Exercise 2
+SELECT
+	Country.Code,
+	Country.Name,
+	City.Name
+FROM
+	Country LEFT JOIN City
+ON
+	Country.Code = City.CountryCode
+WHERE
+	City.Name IS NULL;
+
+--Exercise 3
+SELECT
+	Country.Code,
+	Country.Name
+FROM
+	CountryLanguage RIGHT JOIN Country
+ON
+	Country.Code = CountryLanguage.CountryCode
+WHERE
+	CountryLanguage.Language IS NULL;
+
+
+
+
+
