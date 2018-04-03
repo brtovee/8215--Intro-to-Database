c3b1a092626a53226ec96eaeeee9e0f43cfd00f9
Bruce Tovee <37085952+brtovee@users.noreply.github.com>
2018-04-02 20:32:43 -0400

    Add files via upload

---
+++ Assign2/BruceToveAssignment2_SDG.DDL.sql
@@ -0,0 +1,81 @@
+﻿/*
+File Name: Assignment2_SDG.DDL
+Authors Bruce Tovee
+Date Created: 2018-03-28
+*/
+
+DROP TABLE IF EXISTS Poverty_t;
+DROP TABLE IF EXISTS Indicator_t;
+DROP TABLE IF EXISTS Country_t;
+DROP TABLE IF EXISTS Series_t;
+DROP TABLE IF EXISTS Sex_t;
+DROP TABLE IF EXISTS Age_t;
+
+DROP INDEX IF EXISTS Country_Index;
+DROP INDEX IF EXISTS Poverty_Index;
+
+DROP VIEW IF EXISTS PovertyAgeGroup_View;
+
+CREATE TABLE Indicator_t(
+	Goal VARCHAR(5),
+	Target VARCHAR(3),
+	IndicatorRef VARCHAR(10),
+	IndicatorID VARCHAR(10),
+	IndicatorDescription VARCHAR,
+	CONSTRAINT Indicator_PK PRIMARY KEY(IndicatorID)
+);
+
+CREATE TABLE Sex_t(
+	Sex VARCHAR(50),
+	CONSTRAINT Sex_PK PRIMARY KEY(Sex)
+);
+CREATE TABLE Age_t(
+	AgeGroup VARCHAR,
+	AgeID VARCHAR(4),
+	CONSTRAINT Age_PK PRIMARY KEY(AgeGroup)
+);
+
+CREATE TABLE Country_t(
+	CountryOrAreaCode VARCHAR,
+	CountryOrAreaName VARCHAR
+);
+
+
+CREATE TABLE Series_t(
+	SeriesCode VARCHAR(15),
+	SeriesType VARCHAR(5),
+	SeriesDescription VARCHAR,
+	CONSTRAINT Series_PK PRIMARY KEY(SeriesCode)
+);
+
+
+CREATE TABLE Poverty_t(
+	IndicatorID VARCHAR(10),
+	SeriesCode VARCHAR(15),
+	CountryOrAreaCode VARCHAR,
+	AgeGroup VARCHAR,
+	Sex VARCHAR(50),
+	"2016" DECIMAL(8,3),
+	FN VARCHAR
+);
+
+CREATE INDEX Country_Index
+ON Country_t (CountryOrAreaCode, CountryOrAreaName);
+
+CREATE INDEX Poverty_Index
+ON Poverty_t (CountryOrAreaCode, AgeGroup, Sex, "2016");
+
+CREATE VIEW PovertyAgeGroup_View AS
+SELECT DISTINCT
+	Poverty_t.CountryOrAreaCode AS "Country Code",
+	count(Age_t.AgeGroup) AS "Age Group Count"
+FROM
+	Poverty_t JOIN Age_t
+ON
+	Poverty_t.AgeGroup = Age_t.AgeGroup
+WHERE
+	Poverty_t.Sex = 'Male'
+GROUP BY
+	Poverty_t.CountryOrAreaCode
+HAVING
+	Poverty_t.CountryOrAreaCode LIKE 'C%';
---
+++ Assign2/BruceToveeAssignment2_SDG.DML.sql
@@ -0,0 +1,106 @@
+﻿/*
+File Name: Assignment2_SDG.DML
+Authors Bruce Tovee
+Date Created: 2018-03-28
+*/
+-- 8 a
+SELECT DISTINCT
+	CountryOrAreaCode AS "Country Code",
+	CountryOrAreaName AS "Country Name"
+FROM
+	Country_t
+WHERE
+	CountryOrAreaName LIKE 'C%'
+ORDER BY
+	CountryOrAreaName DESC;
+
+--8 b
+SELECT 
+	CountryOrAreaCode AS "Country Code",
+	"2016"
+FROM
+	Poverty_t
+WHERE 
+	"2016" > 0
+ORDER BY
+	"2016" ASC;
+
+--8 c
+SELECT
+	CountryOrAreaCode AS "Country Code",
+	count(AgeGroup) AS "Age group Count"
+FROM
+	Poverty_t
+GROUP BY
+	CountryOrAreaCode
+ORDER BY
+	CountryOrAreaCode DESC;
+
+-- 8 d
+SELECT DISTINCT
+	CountryOrAreaCode AS "Country Code",
+	Sex AS "Sex"
+FROM
+	Poverty_T
+WHERE
+	Sex IN ('Male', 'Female')
+ORDER BY
+	CountryOrAreaCode DESC
+LIMIT 10;
+
+--9 This query shows which country names have a null value in the 2016 column
+SELECT DISTINCT
+	Country_t.CountryOrAreaName AS "Country Name",
+	Poverty_t."2016"
+FROM
+	Poverty_t LEFT JOIN Country_t
+ON
+	Poverty_t.CountryOrAreaCode = Country_t.CountryOrAreaCode
+WHERE
+	"2016" IS NULL;
+
+
+--10
+SELECT 
+	CountryOrAreaCode 
+FROM 
+	Poverty_t
+WHERE
+	CountryOrAreaCode LIKE 'C%'
+UNION
+SELECT 
+	CountryOrAreaCode 
+FROM 
+	country_t
+WHERE
+	CountryOrAreaCode LIKE 'C%'
+ORDER BY 
+	CountryOrAreaCode;
+
+--11 The purpose of the sub query is to find the max value for 2016 column and then use that max value in the main query
+SELECT DISTINCT
+	Country_t.CountryOrAreaName AS "Country Name",
+	Poverty_t."2016"
+FROM
+	Poverty_t JOIN Country_t
+ON
+	Poverty_t.CountryOrAreaCode = Country_t.CountryOrAreaCode
+WHERE
+	Poverty_t."2016" = (SELECT MAX("2016")
+				FROM Poverty_t);
+
+--12 The purpose of this query is to tell you the amount of age groups used for countries starting with a letters c's male population
+				
+SELECT DISTINCT
+	Poverty_t.CountryOrAreaCode AS "Country Code",
+	count(Age_t.AgeGroup) AS "Age Group Count"
+FROM
+	Poverty_t JOIN Age_t
+ON
+	Poverty_t.AgeGroup = Age_t.AgeGroup
+WHERE
+	Poverty_t.Sex = 'Male'
+GROUP BY
+	Poverty_t.CountryOrAreaCode
+HAVING
+	Poverty_t.CountryOrAreaCode LIKE 'C%';
